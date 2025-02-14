bb33b900e1df02ad1f924863d9fcf76a9be9a970
Kalle Heuwes <karlheinz.heuwes@gmail.com>
2025-02-14 17:54:08 +0100

    20250214c

--- code/pumpe.py
+++ code/pumpe.py
@@ -1,15 +1,22 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
 import sys
 
 if __name__ == "__main__":
     fuelldauer = float(sys.argv[1])
+
 RELAIS_PIN = 4  # GPIO-Pin für das Relais
 
 # GPIO-Modus setzen
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAIS_PIN, GPIO.OUT)
 
+def log(inText):
+    c = datetime.datetime.now()
+    current_time = c.strftime('%H:%M:%S')
+    print(current_time + " " + inText)
+    
 def pumpe_an():
     GPIO.output(RELAIS_PIN, GPIO.HIGH)  # Relais aktivieren (Pumpe EIN)
 
@@ -17,10 +24,10 @@ def pumpe_aus():
     GPIO.output(RELAIS_PIN, GPIO.LOW)   # Relais deaktivieren (Pumpe AUS)
 
 try:
-    print(f"Pumpe startet für {fuelldauer} Sekunden...")
+    log(f"Pumpe startet für {fuelldauer} Sekunden...")
     pumpe_an()
     time.sleep(fuelldauer)
-    print("Pumpe aus")
+    log("Pumpe aus")
     pumpe_aus()
 
 finally:
--- code/stepper.py
+++ code/stepper.py
@@ -4,7 +4,6 @@ import sys
 import datetime
 import subprocess
 
-
 if __name__ == "__main__":
     anzahlPinnchen = int(sys.argv[1])
     fuellmenge = float(sys.argv[2])
@@ -18,10 +17,6 @@ IN1 = 17
 IN2 = 18
 IN3 = 27
 IN4 = 22
-
-# RELAIS_PIN = 4  # GPIO-Pin für die Pumpe
-# GPIO.setup(RELAIS_PIN, GPIO.OUT)
-# GPIO.output(RELAIS_PIN, 0)
 
 # Pin-Setup
 motor_pins = [IN1, IN2, IN3, IN4]
@@ -59,39 +54,12 @@ def motor_steuern(steps, delay=0.001):
                 GPIO.output(pin, val)
             time.sleep(delay)
 
-def pumpe(delay):
-    GPIO.output(RELAIS_PIN, GPIO.HIGH)  # Relais aktivieren (Pumpe EIN)
-    time.sleep(delay)
-    GPIO.output(RELAIS_PIN, GPIO.LOW)   # Relais deaktivieren (Pumpe AUS)
-    GPIO.output(RELAIS_PIN, GPIO.LOW)   # Relais deaktivieren (Pumpe AUS)
-    log("Pumpe aus")
-    
-def pumpe_an():
-    GPIO.output(RELAIS_PIN, GPIO.HIGH)  # Relais aktivieren (Pumpe EIN)
-
-def pumpe_aus():
-    GPIO.output(RELAIS_PIN, GPIO.LOW)   # Relais deaktivieren (Pumpe AUS)
-    
 try:
        
     for x in range(anzahlPinnchen):
         log(f"Befülle Pinnchen {x + 1} mit {fuellmenge} ml für {fuelldauer} Sekunden ...")
-        subprocess.run(["python3", "pumpe.py", "2"])
-        # pumpe(fuelldauer)
-        # time.sleep(2)
-        #time.sleep(fuelldauer)
-        #pumpe_an()
-        #time.sleep(fuelldauer)
-        #print("Pumpe aus")
-        #pumpe_aus()
-        motor_steuern(int(512/ anzahlPinnchen))
-    
-    # print("Motor dreht sich vorwärts...")
-    # motor_steuern(128)  # 512 Schritte = ca. eine Umdrehung
-    # time.sleep(1)
-
-    # print("Motor dreht sich rückwärts...")
-    # motor_steuern(-256)
+        subprocess.run(["python3", "pumpe.py", f"{fuelldauer}"])    # Pumpfunktion in Extrascript ausgelagert, da die Pumpe sonst durchgehend läuft
+        motor_steuern(int(512/ anzahlPinnchen))                     # 512 Schritte = ca. eine Umdrehung
 
 finally:
     GPIO.cleanup()
