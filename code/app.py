d500d778c9c521957c52a152a02114920c440944
Kalle Heuwes <karlheinz.heuwes@gmail.com>
2025-02-18 18:21:25 +0100

    20250218c

--- code/app.py
+++ code/app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/start")
+def start_pumpe():
+    subprocess.Popen(["sudo", "python3", "pumpe.py", "0"])
+    return "Pumpe gestartet"
+
+@app.route("/stop")
+def stop_pumpe():
+    subprocess.Popen(["sudo", "pkill", "-f", "pumpe.py"])
+    return "Pumpe gestoppt"
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
