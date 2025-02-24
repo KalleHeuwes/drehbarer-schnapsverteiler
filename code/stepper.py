c5609aaa3a2386868f2ae042ad1327121cfd9330
Kalle Heuwes <karlheinz.heuwes@gmail.com>
2025-02-24 19:59:07 +0100

    20250224e

--- code/stepper.py
+++ code/stepper.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import datetime
 import sys
@@ -9,49 +9,49 @@ if __name__ == "__main__":
     fuellmenge = float(sys.argv[2])
     fuelldauer = fuellmenge / 10
 
-GPIO.setmode(GPIO.BCM)      # GPIO-Modus setzen
+#GPIO.setmode(GPIO.BCM)      # GPIO-Modus setzen
 
 # Pins für den ULN2003 Motor Driver
-IN1 = 17
-IN2 = 18
-IN3 = 27
-IN4 = 22
+#IN1 = 17
+#IN2 = 18
+#IN3 = 27
+#IN4 = 22
 
 # Pin-Setup
-motor_pins = [IN1, IN2, IN3, IN4]
-for pin in motor_pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, 0)
+#motor_pins = [IN1, IN2, IN3, IN4]
+#for pin in motor_pins:
+#    GPIO.setup(pin, GPIO.OUT)
+#    GPIO.output(pin, 0)
 
 # Schrittsequenz für den Motor (Halbschritt)
-seq = [
-    [1, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
-    [0, 0, 0, 1],
-    [1, 0, 0, 1]
-]
+# seq = [
+#     [1, 0, 0, 0],
+#     [1, 1, 0, 0],
+#     [0, 1, 0, 0],
+#     [0, 1, 1, 0],
+#     [0, 0, 1, 0],
+#     [0, 0, 1, 1],
+#     [0, 0, 0, 1],
+#     [1, 0, 0, 1]
+# ]
 
 def log(inText):
     c = datetime.datetime.now()
     current_time = c.strftime('%H:%M:%S')
     print(current_time + " " + inText)
     
-def motor_steuern_lokal(steps, delay=0.001):
-    """Bewegt den Motor um eine bestimmte Anzahl von Schritten"""
-    log("Fahre zum nächsten Pinnchen ...")
-    if steps < 0:
-        seq.reverse()  # Drehrichtung umkehren
-        steps = -steps
+# def motor_steuern_lokal(steps, delay=0.001):
+#     """Bewegt den Motor um eine bestimmte Anzahl von Schritten"""
+#     log("Fahre zum nächsten Pinnchen ...")
+#     if steps < 0:
+#         seq.reverse()  # Drehrichtung umkehren
+#         steps = -steps
 
-    for _ in range(steps):
-        for step in seq:
-            for pin, val in zip(motor_pins, step):
-                GPIO.output(pin, val)
-            time.sleep(delay)
+#     for _ in range(steps):
+#         for step in seq:
+#             for pin, val in zip(motor_pins, step):
+#                 GPIO.output(pin, val)
+#             time.sleep(delay)
 
 try:
        
@@ -62,4 +62,5 @@ try:
         #motorsteuerung.motor_steuern(int(512/ anzahlPinnchen))                     # 512 Schritte = ca. eine Umdrehung
 
 finally:
-    GPIO.cleanup()
+    #GPIO.cleanup()
+    log("Programmende !")
