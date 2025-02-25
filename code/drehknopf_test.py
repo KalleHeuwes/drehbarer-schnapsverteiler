32ab256a1181f53343ac3e2f35ad08ddb05c611c
Kalle Heuwes <karlheinz.heuwes@gmail.com>
2025-02-25 09:06:38 +0100

    20250225a

--- code/drehknopf_test.py
+++ code/drehknopf_test.py
@@ -1,45 +1,17 @@
-import time
+from gpiozero import RotaryEncoder, Button
+from signal import pause
 
-import RPi.GPIO as GPIO
+# GPIO-Pins für den Encoder (CLK, DT) und den Taster (SW)
+encoder = RotaryEncoder(9, 10, wrap=True)
+button = Button(11)  # GPIO 11 für den Taster
 
-class TurningKnob:
-    def __init__(self, clk_pin, dt_pin):
-        self.clk_pin = clk_pin
-        self.dt_pin = dt_pin
-        self.counter = 0
-        self.clk_last_state = None
+def drehen():
+    print(f"Position: {encoder.value}")
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.clk_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.dt_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+def gedrueckt():
+    print("Knopf gedrückt!")
 
-        self.clk_last_state = GPIO.input(self.clk_pin)
+encoder.when_rotated = drehen
+button.when_pressed = gedrueckt  # Callback für Tastendruck
 
-    def update(self):
-        clk_state = GPIO.input(self.clk_pin)
-        dt_state = GPIO.input(self.dt_pin)
-
-        if clk_state != self.clk_last_state:
-            if dt_state != clk_state:
-                self.counter += 1
-            else:
-                self.counter -= 1
-
-        self.clk_last_state = clk_state
-
-    def get_counter(self):
-        return self.counter
-
-    def cleanup(self):
-        GPIO.cleanup()
-
-if __name__ == "__main__":
-    knob = TurningKnob(clk_pin=17, dt_pin=18)
-
-    try:
-        while True:
-            knob.update()
-            print("Counter:", knob.get_counter())
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        knob.cleanup()
+pause()  # Lässt das Programm laufen
